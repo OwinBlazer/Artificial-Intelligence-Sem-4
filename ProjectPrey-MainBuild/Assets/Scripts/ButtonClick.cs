05cddcded5fe05630342bc835389c33f14f7b579
OwinBlazer <33065589+OwinBlazer@users.noreply.github.com>
2018-05-28 20:19:15 +0700

    Combo of Run And Mate
    Script to run, and mating, as well as looking for mate completed

---
+++ ProjectPrey-MainBuild/Assets/Scripts/ButtonClick.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class ButtonClick : MonoBehaviour {
+
+    public void ChangeScene(string level)
+    {
+        if (level == "Quit")
+        {
+            Application.Quit();
+            Debug.Log("App quit");
+        }
+        SceneManager.LoadScene(level);
+    }
+    
+}

79b84b1c2e2be6b0b409fc79f84281c76a8b3f32
Joseph Ramli <josephramli@github.com>
2018-06-16 14:16:02 +0700

    Highscore
    ass

--- ProjectPrey-MainBuild/Assets/Scripts/Cd_MainGame.cs
+++ ProjectPrey-MainBuild/Assets/Scripts/Cd_MainGame.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Cd_MainGame : MonoBehaviour {
     public float score = 0;
+    public Text scoregameplay;
+    public Text highscoregameplay;
+    public Text highscoremainmenu;
+    public Text scoreresult;
     public int enemyCount = 0;
     private void Start()
     {
-        
+        highscoregameplay.text=PlayerPrefs.GetFloat("HighScore",0).ToString();
     }
     private void Update()
     {
+        
         if (enemyCount <= 0)
         {
             Debug.Log("Game End");
         }
-            
+        
+        scoregameplay.text=score.ToString();
+        
+        if(score>PlayerPrefs.GetFloat("HighScore",0)) 
+        {
+            PlayerPrefs.SetFloat("HighScore",score);
+            highscoregameplay.text=score.ToString();
+        }   
     }
 }
